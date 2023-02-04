ce3d0a25d16f0f8cbc1a99cbbdc3d71d8486c9f3
raxelortiz7 <raxelortiz7@csu.fullerton.edu>
2023-02-04 02:51:32 -0800

    yes

---
+++ contacts.py
@@ -0,0 +1,21 @@
+def print_list(contact_list):
+    print(f'{"Index":8}{"First name":22}{"Last name":22}')
+    print("======  ====================  ==================== ")
+    for i in range(len(contact_list)):
+        print(f'{str(i):8}{contact_list[i][0]:22}{contact_list[i][1]:22}') 
+        
+def add_contacts(contact_list, first_name, last_name): contact_list.append([first_name, last_name])
+
+def modify_contacts(contact_list, first_name, last_name, i):
+    if i < len(contact_list):
+        contact_list[i] = [first_name, last_name] 
+        return True
+    else: 
+        return False
+
+def delete_contacts(contact_list, i):
+    if i < len(contact_list): 
+        del(contact_list[i])
+        return True
+    else: 
+        return False
---
+++ main.py
@@ -0,0 +1,38 @@
+import contacts
+
+print("      *** EMPLOYEE CONTACT MAIN MENU \n")
+print("1. Print list\n2. Add contact\n3. Modify Contact\n4. Delete Contact\n5. Exit")
+contact_list=[]
+while True:
+    response = input('\nEnter menu choice: ')
+    if response == '1':
+        print("================== CONTACT LIST ==================\n")
+        contacts.print_list(contact_list)
+        
+    elif response == '2':
+        first_name = input("\nEnter Frist Name: ")
+        last_name = input("Enter Last Name: ") 
+        contacts.add_contacts(contact_list, first_name, last_name)
+        
+    elif response == '3':
+        i = int(input("\nEnter the index number:"))
+        first_name = input("\nEnter New Frist Name: ")
+        last_name = input("Enter New Last Name: ")
+        changed = contacts.modify_contacts(contact_list, first_name, last_name, i)
+        if not changed:
+            print("Invalid index number")
+            
+    elif response == '4':
+        i = int(input("\nEnter index number: "))
+        changed = contacts.delete_contacts(contact_list, i)
+        if not changed:
+            print("Invalid index number")
+            
+    elif response == '5':
+        break
+    
+    else:
+        print("Invalid response -_-")
+        break
+
+    

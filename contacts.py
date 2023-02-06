1d39540d2c48f0cedd890c5fe2df75ac5a59d86a
raxelortiz7 <raxelortiz7@csu.fullerton.edu>
2023-02-05 18:44:42 -0800

    implemented all functions in contact.py and added doc strings and no longer trolling

--- contacts.py
+++ contacts.py
@@ -1,21 +1,50 @@
 '''
 Raxel Ortiz - 02-04-23 - contacts.py is a file that stores functions that modify out contacts table that is stored as a list 
 '''
+contact_list = []
 def print_list(contact_list):
+    '''
+    Print each index of the list using a for loop
+    prints out the first and second index of each item since they 
+    lists as well
+    '''
+    print("\n================== CONTACT LIST ==================")
     print(f'{"Index":8}{"First name":22}{"Last name":22}')
     print("======  ====================  ==================== ")
     for i in range(len(contact_list)): print(f'{str(i):8}{contact_list[i][0]:22}{contact_list[i][1]:22}') 
         
-def add_contacts(contact_list, first_name, last_name): contact_list.append([first_name, last_name])
+def add_contacts(contact_list):
+    '''
+    Takes a list as a parameter. User is asked for first name
+    and last name usint input()
+    from there imput is placed into index 0, and 1 (stored as a list of 2 indexes)
+    and appended on to the main list 
+    '''
+    first_name, last_name = input("\nEnter Frist Name: "), input("Enter Last Name: ")
+    contact_list.append([first_name, last_name])
+    return contact_list
 
-def modify_contacts(contact_list, first_name, last_name, i):
+def modify_contacts(contact_list):
+    '''
+    ask the user what index they want to change
+    if the index is in range we will take the new information
+    and update the list 
+    if not we well respond with an invalid index
+    '''
+    i = int(input("\nEnter the index number:"))
     if i < len(contact_list):
+        first_name = input("\nEnter New Frist Name: ")
+        last_name = input("Enter New Last Name: ")
         contact_list[i] = [first_name, last_name] 
         return True
-    else: 
-        return False
 
-def delete_contacts(contact_list, i):
+def delete_contacts(contact_list):
+    '''
+    Takes user input for what needs to be deleted 
+    if index in range we will update list else we will 
+    return invalid index
+    '''
+    i = int(input("\nEnter index number: "))
     if i < len(contact_list): 
         del(contact_list[i])
         return True
--- main.py
+++ main.py
@@ -1,38 +1,36 @@
+'''
+Raxel Ortiz - 02-04-23 - main.py is a file that calls functions that modify out contacts table that is stored as a list 
+'''
 import contacts
 
-print("      *** EMPLOYEE CONTACT MAIN MENU \n")
+print("\n      *** EMPLOYEE CONTACT MAIN MENU \n")
 print("1. Print list\n2. Add contact\n3. Modify Contact\n4. Delete Contact\n5. Exit")
-contact_list=[]
+
+contact_list = []
+
 while True:
     response = input('\nEnter menu choice: ')
     if response == '1':
-        print("================== CONTACT LIST ==================")
         contacts.print_list(contact_list)
         
     elif response == '2':
-        first_name = input("\nEnter Frist Name: ")
-        last_name = input("Enter Last Name: ") 
-        contacts.add_contacts(contact_list, first_name, last_name)
-        
+        contacts.add_contacts(contact_list)
+    
     elif response == '3':
-        i = int(input("\nEnter the index number:"))
-        first_name = input("\nEnter New Frist Name: ")
-        last_name = input("Enter New Last Name: ")
-        changed = contacts.modify_contacts(contact_list, first_name, last_name, i)
-        if not changed:
+        updated = contacts.modify_contacts(contact_list)
+        if not updated:
             print("Invalid index number")
             
     elif response == '4':
-        i = int(input("\nEnter index number: "))
-        changed = contacts.delete_contacts(contact_list, i)
-        if not changed:
+        updated = contacts.delete_contacts(contact_list)
+        if not updated:
             print("Invalid index number")
             
     elif response == '5':
         break
     
     else:
-        print("Invalid response -_-")
-        break
+        print("Invalid index try again")
+        
 
     
